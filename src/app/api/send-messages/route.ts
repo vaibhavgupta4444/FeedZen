48d009cebc56704521af62376a5a2cbb29709917
vaibhavInnovation <vaibhav.ayushgupta@gmail.com>
2025-07-31 17:35:45 +0530

    website for taking anonymous feedback

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,42 @@
+import dbConnect from "@/lib/dbConnect"
+import UserModel from "@/models/User"
+import { Message } from "@/models/User"
+
+export async function POST(request: Request) {
+    await dbConnect();
+    const { username, content } = await request.json();
+
+    try {
+        const user = await UserModel.findOne({ username });
+        if (!user) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, { status: 404 })
+        }
+
+        if (!user.isAcceptingMessage) {
+            return Response.json({
+                success: false,
+                message: "Form is currently closed"
+            }, { status: 403 })
+        }
+
+        const newMessage = { content, createdAt: new Date() }
+        user.messages.push(newMessage as Message)
+        await user.save()
+
+        return Response.json({
+                success: true,
+                message: "Feedback send successfully"
+            }, { status: 200 })
+
+    } catch (error) {
+        return Response.json({
+            success: false,
+            message: "Internal server error"
+        }, {
+            status: 500
+        })
+    }
+}
